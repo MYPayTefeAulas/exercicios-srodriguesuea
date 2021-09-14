78d28636c30ba93526adc28076821107a0505e95
Sabrina <srodrigues@uea.edu.br>
2021-09-14 10:01:26 -0400

    adiciona layout exercicio greeter 1

---
+++ MyPatas/app/src/main/java/com/example/mypatas/Greeter1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.mypatas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Greeter1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greeter1)
+    }
+}
